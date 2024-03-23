9b63e9d183c92574224e68136370cdef5bdf35b6
Alexandre Gvozdenovic <alexandregvozdenovic@revolugo.com>
2024-03-23 17:19:54 +0100

    fixed issue with api changed

--- hooks/useCharacters.tsx
+++ hooks/useCharacters.tsx
@@ -1,7 +1,6 @@
 import useSWR from "swr";
 
 export function useCharacters() {
-  const { data, error } = useSWR(`https://hp-api.herokuapp.com/api/characters`);
-
+  const { data, error } = useSWR(`https://hp-api.onrender.com/api/characters`);
   return { characters: data, isLoading: !error && !data, isError: error };
 }
